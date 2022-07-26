dea25ee123a825af68109a77e504a3b6ad0905cb
DeveloperDulli <dulli1616@kakao.com>
2022-07-26 15:33:50 +0900

    07.26

--- src/main/java/com/kiri/controller/AdminController.java
+++ src/main/java/com/kiri/controller/AdminController.java
@@ -153,10 +153,6 @@ public class AdminController {
 		return "redirect:/admin/toGroupAdmin?curPage=1";
 	}
 	
-	@RequestMapping(value = "/toAdmin")
-	public String toAdmin() {
-		return "admin/adminMain";
-	}
 	
 	@RequestMapping(value = "/toError")
 	public String toError() {
